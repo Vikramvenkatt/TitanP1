e95c631c79aaed56ab01a239ceafc93c1ce5efa2
Jan-04 <72268860+Jan-04@users.noreply.github.com>
2021-03-15 14:13:07 +0100

    Add files via upload

--- src/GenBody/Body.java
+++ src/GenBody/Body.java
@@ -4,7 +4,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Vector;
 
-public class Body {
+public class Body{
     /*so we are gonna need some global variables that define the parameters of the body of the spaceship
     such as name, mass,color etc
     idk how many we need tho and whether stuff like colour is important but i think it prob is
@@ -15,7 +15,7 @@ public class Body {
     public double Radius;
     public double mass;
     public Vector position;//is of type Vector class, which was created drawing from the interface provided in API
-    public Vector Velocity;//same thing I guess
+    public Vector velocity;//same thing I guess
     public Color BColor;
 
     /*now we have to create methods that simply return all of the above
@@ -29,19 +29,28 @@ public class Body {
         return mass;
     }
     public Vector getVelocity(){
-        return Velocity;
+        return velocity;
     }
     public Vector getPosition(){
         return position;
     }
+	public String getName(){return BName;}
+	
+	public Color getColor(){return BColor;}
+	
     //Constructor- Sahil
-    public Body(){
-
+    public Body(Bdata data){
+		this.BName = data.getName();
+        this.mass = data.getMass();
+        this.Radius = data.getRadius();
+        this.position = data.getInitialPosition();
+        this.velocity = data.getInitialVelocity();
+        this.BColor = data.getColor();
     }
     /*now we need a few more methods but idk what they are
     Both Vikram and Sahil will work on this method
      */
-
+	
 
 
 }
