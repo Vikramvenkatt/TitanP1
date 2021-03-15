7a0b8a3334f428668c0ecd02ef059ad1be2b77c9
emilioemre <47246502+emilioemre@users.noreply.github.com>
2021-03-15 21:02:30 +0100

    Add files via upload

---
+++ src/Planet.java
@@ -0,0 +1,19 @@
+package titan;
+
+import java.awt.*;
+public class Planet extends Bdata {
+	public Planet(String name, double mass, Vector initialPosition, Vector initialVelocity,double radius , Color Color)
+	/**
+	 * Needs a fix . 
+	 * 
+	 */
+	
+	{
+		super(name, mass, initialPosition, initialVelocity, radius , Color );
+
+	}
+
+
+
+}
+ 
---
+++ src/Planets.java
@@ -0,0 +1,90 @@
+package titan;
+
+
+import java.awt.*;
+import java.util.ArrayList;
+
+public class Planets {
+	
+	private Planet sun;
+	private Planet venus;
+	private Planet mercury;
+	private Planet jupiter;
+	private Planet mars;
+	private Planet earth;
+	private Planet uranus;
+	private Planet saturn;
+	private Planet titan;
+	
+	private ArrayList<Planet> planets;
+
+	public Planet getSun() {
+		return sun;
+	}
+
+	public Planet getVenus() {
+		return venus;
+	}
+
+	public Planet getMercury() {
+		return mercury;
+	}
+
+	public Planet getJupiter() {
+		return jupiter;
+	}
+
+	public Planet getMars() {
+		return mars;
+	}
+
+	public Planet getEarth() {
+		return earth;
+	}
+
+	public Planet getUranus() {
+		return uranus;
+	}
+
+	public Planet getSaturn() {
+		return saturn;
+	}
+
+	public Planet getTitan() {
+		return titan;
+	}
+
+	public ArrayList<Planet> getPlanets() {
+		return planets;
+	}
+
+	public void addPlanets()
+	{
+		//Create a new "planet" (Sun is a star but who cares) with the given parameters of the text file given to us
+		//the vectors need to be added later so set them all to null for now and set appropriate colors for the planets
+		//for the GUI team, so Color.blue for earth
+		sun = new Planet("Sun",1.9891e30, null, null,6.957e8, Color.yellow);
+		venus = new Planet("Venus",4.8685e24,null,null,6.0518e6, Color.white);
+		mercury = new Planet("Mercury", 3.302e23, null, null, 2.44e6, Color.GRAY);
+		jupiter = new Planet("Jupiter", 1.89813e27, null, null, 6.99e7, Color.orange);
+		mars = new Planet("Mars", 6.4171e23, null, null, 3.39e6, Color.red);
+		earth = new Planet("Earth", 5.97219e24, null, null, 6371e3, Color.BLUE);
+		uranus = new Planet("Uranus", 8.6813e25, null, null, 2.54e7, Color.green);
+		saturn = new Planet("Saturn", 5.6834e26, null , null, 5.82e7, Color.yellow);
+		titan = new Planet("Titan", 1.34553e23, null, null, 2.575e6, Color.orange);
+		
+		
+		planets.add(sun);
+		planets.add(venus);
+		planets.add(mercury);
+		planets.add(jupiter);
+		planets.add(mars);
+		planets.add(earth);
+		planets.add(uranus);
+		planets.add(saturn);
+		planets.add(titan);
+		
+		
+	}
+	
+}
