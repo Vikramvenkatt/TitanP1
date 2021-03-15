16f074fe81d5037bf4e554aa861146f4f701692c
KaiNi21 <kaiw@live.fr>
2021-03-15 14:23:36 +0100

    Created Spaceship class Added simple and complete constructor

--- src/GenBody/Body.java
+++ src/GenBody/Body.java
@@ -11,32 +11,47 @@ public class Body {
     Vikram
      */
     //so this is just copied from what I wrote from the Data body class
-    public String BName;//name of the body, like in general, we have spaceship defined in another class
-    public double Radius;
+    public String bodyName;//name of the body, like in general, we have spaceship defined in another class
+    public double radius;
     public double mass;
     public Vector position;//is of type Vector class, which was created drawing from the interface provided in API
-    public Vector Velocity;//same thing I guess
-    public Color BColor;
+    public Vector velocity;//same thing I guess
+    public Color color;
 
     /*now we have to create methods that simply return all of the above
     Vikram
      */
 
     public double getRadius(){
-        return Radius;
+        return radius;
     }
     public double getMass(){
         return mass;
     }
     public Vector getVelocity(){
-        return Velocity;
+        return velocity;
     }
     public Vector getPosition(){
         return position;
     }
-    //Constructor- Sahil
-    public Body(){
 
+    //simple constructor which requires less parameters for testing phase 1
+    public Body(double mass, Vector position, Vector velocity)
+    {
+        this.mass = mass;
+        this.position = position;
+        this.velocity = velocity;
+    }
+
+    //complete constructor
+    public Body(String bodyName, double radius, double mass, Vector position, Vector velocity, Color color)
+    {
+        this.bodyName = bodyName;
+        this.radius = radius;
+        this.mass = mass;
+        this.position = position;
+        this.velocity = velocity;
+        this.color = color;
     }
     /*now we need a few more methods but idk what they are
     Both Vikram and Sahil will work on this method
---
+++ src/GenBody/Spaceship.java
@@ -0,0 +1,4 @@
+package GenBody;
+
+public class Spaceship {
+}
