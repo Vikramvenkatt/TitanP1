c108313a5fbbc0b12061960091ccf208d21cb130
KaiNi21 <kaiw@live.fr>
2021-03-16 12:48:02 +0100

    Added import, changed constructor

--- src/Planet.java
+++ src/Planet.java
@@ -1,14 +1,12 @@
 import java.awt.*;
 import java.util.Vector;
+import GenBody.Bdata;
 public class Planet extends Bdata {
 
 	public Planet(Bdata orbitsAround, String name, double mass, Vector initialPosition, Vector initialVelocity,
-			double radius, java.awt.Color Color) {
-		super(orbitsAround, name, mass, initialPosition, initialVelocity, radius, Color);
+			double radius, Color color) {
+		super(orbitsAround, name, mass, initialPosition, initialVelocity, radius, color);
 	}
-
-	
-
 
 }
  
