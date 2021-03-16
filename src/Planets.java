dfb6e9d4822bef644e2a7707b7075e9210997a62
emilioemre <47246502+emilioemre@users.noreply.github.com>
2021-03-16 01:08:35 +0100

    Update Planets.java

--- src/Planets.java
+++ src/Planets.java
@@ -1,6 +1,3 @@
-package titan;
-
-
 import java.awt.*;
 import java.util.ArrayList;
 
@@ -63,15 +60,15 @@ public class Planets {
 		//Create a new "planet" (Sun is a star but who cares) with the given parameters of the text file given to us
 		//the vectors need to be added later so set them all to null for now and set appropriate colors for the planets
 		//for the GUI team, so Color.blue for earth
-		sun = new Planet("Sun",1.9891e30, null, null,6.957e8, Color.yellow);
-		venus = new Planet("Venus",4.8685e24,null,null,6.0518e6, Color.white);
-		mercury = new Planet("Mercury", 3.302e23, null, null, 2.44e6, Color.GRAY);
-		jupiter = new Planet("Jupiter", 1.89813e27, null, null, 6.99e7, Color.orange);
-		mars = new Planet("Mars", 6.4171e23, null, null, 3.39e6, Color.red);
-		earth = new Planet("Earth", 5.97219e24, null, null, 6371e3, Color.BLUE);
-		uranus = new Planet("Uranus", 8.6813e25, null, null, 2.54e7, Color.green);
-		saturn = new Planet("Saturn", 5.6834e26, null , null, 5.82e7, Color.yellow);
-		titan = new Planet("Titan", 1.34553e23, null, null, 2.575e6, Color.orange);
+		sun = new Planet(null, "Sun",1.9891e30, null, null,6.957e8, Color.yellow);
+		venus = new Planet(null, "Venus",4.8685e24,null,null,6.0518e6, Color.white);
+		mercury = new Planet(null, "Mercury", 3.302e23, null, null, 2.44e6, Color.GRAY);
+		jupiter = new Planet(null, "Jupiter", 1.89813e27, null, null, 6.99e7, Color.orange);
+		mars = new Planet(null, "Mars", 6.4171e23, null, null, 3.39e6, Color.red);
+		earth = new Planet(null, "Earth", 5.97219e24, null, null, 6371e3, Color.BLUE);
+		uranus = new Planet(null, "Uranus", 8.6813e25, null, null, 2.54e7, Color.green);
+		saturn = new Planet(null, "Saturn", 5.6834e26, null , null, 5.82e7, Color.yellow);
+		titan = new Planet(null, "Titan", 1.34553e23, null, null, 2.575e6, Color.orange);
 		
 		
 		planets.add(sun);
