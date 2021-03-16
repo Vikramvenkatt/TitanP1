5b9a94fc375dc20ca51abb48633a699389257e84
emilioemre <47246502+emilioemre@users.noreply.github.com>
2021-03-16 13:40:21 +0100

    Update Planet.java

--- src/Planet.java
+++ src/Planet.java
@@ -1,14 +1,12 @@
-import java.awt.*;
-import java.util.Vector;
 public class Planet extends Bdata {
 
 	public Planet(Bdata orbitsAround, String name, double mass, Vector initialPosition, Vector initialVelocity,
 			double radius, java.awt.Color Color) {
 		super(orbitsAround, name, mass, initialPosition, initialVelocity, radius, Color);
+		// TODO Auto-generated constructor stub
 	}
 
+
 	
-
-
 }
  
