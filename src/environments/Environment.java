df8ff273947b67743137b9af1aa51ceb2f5a766e
Vikramven <68946012+Vikramven@users.noreply.github.com>
2021-03-16 20:18:55 +0400

    Add files via upload

---
+++ src/environments/Environment.java
@@ -0,0 +1,28 @@
+package environments;
+import GenBody.Body;
+import GenBody.Planet;
+import GenBody.Planets;
+import GenBody.Spaceship;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+public class Environment {
+    List<Spaceship> spaceShip=new ArrayList<>();//contains the list of spaceships
+    List<Body> all=new ArrayList<>();
+
+    boolean running=false;
+    Thread runner=null;
+    Calendar startDate;
+    long start;
+    Calendar current;
+    private Planets planetsList;
+
+    public void paintComponent(Graphics2D g){
+        planetsList.addPlanets();
+        planetsList.draw(g);
+        //What is completed till now
+    }
+}
