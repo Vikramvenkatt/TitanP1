36b1fbf7a056895fab28306f1587e03cf1d22337
Vikramven <68946012+Vikramven@users.noreply.github.com>
2021-03-16 20:17:12 +0400

    Update Planet.java

--- src/GenBody/Planet.java
+++ src/GenBody/Planet.java
@@ -5,7 +5,16 @@ public class Planet extends Bdata {
 	public Planet(Bdata orbitsAround, String name, double mass, Vector initialPosition, Vector initialVelocity,
 			double radius, java.awt.Color Color) {
 		super(orbitsAround, name, mass, initialPosition, initialVelocity, radius, Color);
+		
+			
 	}
+	    public Color getColor(){
+        return Color;
+    }
+	    public void draw(Graphics g){
+        g.setColor(this.getColor());
+        g.fillOval((int)initialPosition.getX(),(int)initialPosition.getY(),(int)getRadius()*2,(int)getRadius()*2);
+    }
 
 
 
