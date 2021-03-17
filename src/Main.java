5e002124bfece9d90e67f0c70cba5334896225b9
KaiNi21 <kaiw@live.fr>
2021-03-17 14:51:31 +0100

    Added GUI methods

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
+import gui.Environment;
 import gui.UserInterface;
+import java.awt.Graphics;
 
 public class Main {
     public static void main(String[] args) {
         UserInterface ui = new UserInterface();
+        Environment e = new Environment();
+        ui.setEnvironment(e);
         ui.setVisible(true);
+        ui.repaint();
     }
 }
--- src/gui/Environment.java
+++ src/gui/Environment.java
@@ -10,7 +10,7 @@ import java.util.Calendar;
 import java.util.List;
 
 public class Environment {
-    private Spaceship ship = new Spaceship(new Vector(,2,3),new Vector(1,2,3));
+    private Spaceship ship;
     private Planets planetsList;
     boolean running=false;
     Thread runner=null;
@@ -19,12 +19,14 @@ public class Environment {
     Calendar current;
 
     public void paintComponent(Graphics g){
+        //Adds all our planets and calls every individual draw from each planet
         planetsList.addPlanets();
         planetsList.draw(g);
+        //draw the ship at the initial position
         ship.draw(g,(int) ship.getPosition().getX(), (int) ship.getPosition().getY());
     }
 
-
+    //this method should be called before paintComponent as it sets the position of our ship on earth
     public void setLaunchInfo(Vector startPosition)
     {
         ship.setLaunchData((int) startPosition.getX(),(int) startPosition.getY());
