697d4c4ae44dce28de34050f762502e547d10f57
KaiNi21 <kaiw@live.fr>
2021-03-17 13:57:01 +0100

    Created Draw Method for Rocket

--- src/GenBody/Spaceship.java
+++ src/GenBody/Spaceship.java
@@ -1,9 +1,19 @@
 package GenBody;
 
+import java.awt.Graphics;
+import java.awt.Color;
+
 public class Spaceship extends Body{
     //create simple spaceship with no mass, only vectors
     public Spaceship(Vector position, Vector velocity)
     {
         super(position, velocity);
+    }
+
+    //draw a rocket at position x,y
+    public void draw(Graphics g, int x, int y)
+    {
+        g.setColor(Color.red);
+        g.fillRect(x,y, 20,40);
     }
 }
