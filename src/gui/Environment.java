4883ff1f8f49ef038e7f16bb63e8bc113ee021d6
KaiNi21 <kaiw@live.fr>
2021-03-18 22:34:10 +0100

    Updated GUI

--- src/gui/Environment.java
+++ src/gui/Environment.java
@@ -1,35 +1,71 @@
 package gui;
+
 import GenBody.Planet;
 import GenBody.Planets;
 import GenBody.Spaceship;
 import GenBody.Vector;
-import GenBody.LaunchData;
+
+import javax.swing.*;
 import java.awt.*;
-import java.util.ArrayList;
+import java.awt.image.BufferStrategy;
 import java.util.Calendar;
 import java.util.List;
 
-public class Environment {
+public class Environment extends Canvas  {
     private Spaceship ship;
-    private Planets planetsList;
-    boolean running=false;
-    Thread runner=null;
+    private List<Planet> planetsList;
+    boolean running = false;
+    Thread runner = null;
     Calendar startDate;
     long start;
     Calendar current;
 
-    public void paintComponent(Graphics g){
-        //Adds all our planets and calls every individual draw from each planet
-        planetsList.addPlanets();
-        planetsList.draw(g);
-        //draw the ship at the initial position
-        ship.draw(g,(int) ship.getPosition().getX(), (int) ship.getPosition().getY());
+    public Environment(){
+        Planets planets = new Planets();
+        planets.addPlanets();
+        this.planetsList=planets.getPlanets();
     }
 
+
+    public void drawplease(Graphics g) {
+        //Adds all our planets and calls every individual draw from each planet
+//        g.setColor(Color.WHITE);
+//        g.drawString("hello",100,100);
+
+
+        for (Planet planet : this.planetsList) {
+            planet.draw(g);
+
+        }
+
+        //draw the ship at the initial position
+        //ship.draw(g, (int) ship.getPosition().getX(), (int) ship.getPosition().getY());
+    }
+
+    public void run(){
+
+        BufferStrategy bs = this.getBufferStrategy();
+        if (bs == null) {
+            this.createBufferStrategy(3);
+            return;
+        }
+
+        Graphics g = bs.getDrawGraphics();
+
+        g.setColor(Color.BLACK);
+        g.fillRect(0, 0, 1500, 1000);
+
+        this.drawplease(g);
+
+        g.dispose();
+        bs.show();
+
+    }
+
+
     //this method should be called before paintComponent as it sets the position of our ship on earth
-    public void setLaunchInfo(Vector startPosition)
-    {
-        ship.setLaunchData(new Vector((int) startPosition.getX(),(int) startPosition.getY(),0));
+    public void setLaunchInfo(Vector startPosition) {
+        ship.setLaunchData(new Vector((int) startPosition.getX(), (int) startPosition.getY(), 0));
     }
 
 
