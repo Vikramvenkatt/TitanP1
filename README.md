16d694a5ac51b3bcb780131f2bfc2d7b1437b6bb
KaiNi21 <kaiw@live.fr>
2021-03-18 17:39:15 +0100

    Updated ReadMe

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # TitanP1
-Phase 1 of space mission and landing
+Phase 1 of Space mission and landing
+Description: Second panel that contains the background and runs the simulation(action listener)
+            Within the second panel: 3 buttons, click to run, zoom in and out
+            
+To run the simulation without the GUI compile the Main class with:
+"javac Main.java"
+then execute it with
+"java Main"
+the initial position and angle of the spaceship are already put in.
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,23 @@
+import GenBody.Planets;
+import GenBody.Vector;
 import gui.Environment;
 import gui.UserInterface;
 import java.awt.Graphics;
+import GenBody.Simulation;
 
 public class Main {
+    private static Simulation sim = new Simulation();
+    private static Planets planets = new Planets();
     public static void main(String[] args) {
         UserInterface ui = new UserInterface();
         Environment e = new Environment();
         ui.setEnvironment(e);
         ui.setVisible(true);
         ui.repaint();
+        sim.trajectory(new Vector(planets.getEarth().getInitialPosition().getX()+planets.getEarth().getRadius(),
+                planets.getEarth().getInitialPosition().getY()+planets.getEarth().getRadius(),
+                planets.getEarth().getInitialPosition().getZ()+planets.getEarth().getRadius())
+                ,new Vector(60,30,100),
+                100, 10);
     }
 }
