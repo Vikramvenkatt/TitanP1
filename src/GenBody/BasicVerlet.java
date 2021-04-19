d3dbfef489e9f15a77f00695afca507762dddf8f
Vikramven <vikramvenkat2002@gmail.com>
2021-04-19 12:46:20 +0200

    added new class, methods inside

--- src/GenBody/BasicVerlet.java
+++ src/GenBody/BasicVerlet.java
@@ -1,2 +1,13 @@
-package GenBody;public class BasicVerlet {
+package GenBody;
+
+public class BasicVerlet {
+    //base class
+
+    public double time;
+    public double vel;
+
+    public BasicVerlet(double time, double vel) {
+        this.time = time;
+        this.vel = vel;
+    }
 }
