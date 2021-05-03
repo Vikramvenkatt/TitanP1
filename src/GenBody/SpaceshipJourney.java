2d0e02f359d7acaf432973305cadb4cdaf323105
Vikramven <vikramvenkat2002@gmail.com>
2021-05-03 17:18:01 +0200

    separate class for spaceship position

--- src/GenBody/SpaceshipJourney.java
+++ src/GenBody/SpaceshipJourney.java
@@ -2,9 +2,9 @@ package GenBody;
 
 public class SpaceshipJourney {
     /*
-    take out spaceship elemet from the arraylist, so that position can be calculated sparetly
+    take out spaceship element from the arraylist, so that position can be calculated separately
     as of now, make a separate method just for position of spaceship, and take out of for loop
-    fuel efficiency aslo depends on this class, create a variable inside spaceship
+    fuel efficiency also depends on this class, create a variable inside spaceship
     variable for fuel
      */
 }
