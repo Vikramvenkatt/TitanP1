85543a77be44bf74b2f047ffe9fac503d267b4a0
KaiNi21 <kaiw@live.fr>
2021-06-11 14:13:30 +0200

    Added Phase3 Landing classes

---
+++ src/landing/LandingModule.java
@@ -0,0 +1,36 @@
+package landing;
+
+public class LandingModule {
+    //gravity in m/s^2, for reference it's LOWER than earth's
+    protected double g = 1.352;
+
+    //mass of lander according to announcement
+    protected double massOfLander = 6000; //kg
+    protected double massOfFuel = 1000;
+
+    protected double mainThrusters; //aft thrusters/rear thrusters
+    protected double sideThrusters; //we have 4 side thrusters
+    private double dt; //deltaTime
+
+    protected double totalMass = massOfFuel +massOfLander;
+
+    protected boolean isLanded = false;
+    
+    public void calculateMass()
+    {
+        //NOTE: sideThrusters can't generate NEGATIVE thrust, so only two sideThrusters SHOULD be accounted for
+        double totalThrust = Math.abs(mainThrusters) + Math.abs(sideThrusters)*4;
+        //calculate spent fuel by calculating how much fuel has been burned
+        //TODO: FORMULA NEEDS ADJUSTMENT
+        double lostFuel = totalThrust*dt;
+        massOfFuel = massOfFuel - lostFuel;
+        if(massOfFuel <= 0)
+        {
+            System.out.println("No fuel left");
+            massOfFuel = 0;
+        }
+        totalMass = massOfLander + massOfFuel;
+    }
+
+
+}
---
+++ src/landing/StateOfModule.java
@@ -0,0 +1,8 @@
+package landing;
+
+import interfaces.Vector3dInterface;
+
+public class StateOfModule {
+    private Vector3dInterface xPosition;
+    private Vector3dInterface yPosition;
+}
