69d8863d443ef1fd5738ee276325fa9bde7828bd
KaiNi21 <kaiw@live.fr>
2021-06-13 14:36:33 +0200

    Vector class : Added a new method WindModel/L

--- src/landing/WindModel.java
+++ src/landing/WindModel.java
@@ -1,5 +1,8 @@
 package landing;
 
+import GenBody.Vector;
+
+import java.util.ArrayList;
 import java.util.Random;
 
 /**
@@ -14,12 +17,13 @@ import java.util.Random;
  */
 class WindModel extends LandingModule {
     private double g = 1.352; // m / s^2  gravity on Titan
-    private double areaContact = 50; // m^2
-    private double atmosphereDensity = 1.2*1.6; // kg/m^3
+    private double areaContact = 50; // m^2 //TODO: mention in next project meeting
+    private double atmosphereDensity = 1.2*1.19; // kg/m^3
+    private double massOfTitan = 1.34553e23;
+    private double unappliedWindSpeed; // overall mean wind speed
+    private double appliedWindSpeed; // current wind speed after noise has been applied
+    private double windAcceleration; // acceleration caused by wind, only for appliedWindSpeed
 
-    private double meanWindSpeed; // overall mean wind speed
-    private double windSpeedCurrent; // current wind speed after noise has been applied
-    private double windAcceleration; // acceleration caused by wind
 
     public WindModel(){
         windFirstInitialization();
@@ -29,9 +33,9 @@ class WindModel extends LandingModule {
 
     public void windFirstInitialization() {
         // random wind speed ranges form -20 to 20 meters per seconds;
-        if(meanWindSpeed!=-10) {
+        if(unappliedWindSpeed!=-10) {
             Random rand = new Random();
-            meanWindSpeed = rand.nextInt(41) - 20;;
+            unappliedWindSpeed = rand.nextInt(41) - 20;;
         }
     }
 
@@ -39,9 +43,9 @@ class WindModel extends LandingModule {
     public void windForceOnLander() {
         // random wind noise ranges from -0.5  to 0.5
         double windNoise = Math.random()-0.5;
-        windSpeedCurrent = meanWindSpeed * windNoise;
+        appliedWindSpeed = unappliedWindSpeed * windNoise;
         // force = contact area * atmosphere density * windSpeed
-        double windForce = areaContact * atmosphereDensity * windSpeedCurrent;
+        double windForce = areaContact * atmosphereDensity * appliedWindSpeed;
         windAcceleration = windForce/totalMass;
         //System.out.println("Landing acceleration: " + acceleration.getX());
         //System.out.println("Wind acceleration: " + windAcc);
@@ -54,18 +58,38 @@ class WindModel extends LandingModule {
     public double getWindAcc() {
         // random wind noise ranges from -0.5 to 0.5
         double windNoise = Math.random()-0.5;
-        windSpeedCurrent = meanWindSpeed * windNoise;
+        appliedWindSpeed = unappliedWindSpeed * windNoise;
         // force = area of contact * air density * windSpeed
-        double windForce = areaContact * atmosphereDensity * windSpeedCurrent;
+        double windForce = areaContact * atmosphereDensity * appliedWindSpeed;
         return windAcceleration = windForce/totalMass;
     }
 
-    public double getWindSpeedCurrent() {
-        return windSpeedCurrent;
+    public double getappliedWindSpeed() {
+        return appliedWindSpeed;
     }
 
-    public double getMeanWindSpeed() {
-        return meanWindSpeed;
+    public double getunappliedWindSpeed() {
+        return unappliedWindSpeed;
+    }
+
+
+    //TODO: Titan's position is given ONCE, it should actually be
+    /**
+        Applies gravitation force on the landing module, will be called every time step
+        IF THIS DOESNT WORK JUST USE G
+     */
+    public Vector gravitationalForces(Vector positionOfTitan) {
+        Vector gForces = new Vector(); // reset the acceleration
+        // get the relative position to the reference object
+        Vector r = super.position.substract(positionOfTitan);
+        // radius of distance
+        double dist = r.length();
+        // F(m1<-m2) = (g * m1 * m2) / r^2
+        double netForce = (g * super.massOfLander * massOfTitan) / Math.pow(dist, 2);// create the force vector of between the two objects. It is in the direction of
+        // the relative position of the object and the magnitude of the net force
+        Vector thisForce = (Vector) super.acceleration.createUnitVector().mul(netForce);// add force to total acceleration
+        gForces = gForces.add(thisForce);
+        return gForces;
     }
 
 
