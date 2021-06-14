559be7ba029657377a49c5fea637738646392056
Roman <roma020301@gmail.com>
2021-06-14 10:54:37 +0200

    Fixed the spaceship view

--- src/gui/Environment.java
+++ src/gui/Environment.java
@@ -11,7 +11,7 @@ import java.sql.SQLOutput;
 import java.util.Calendar;
 import java.util.List;
 
-public class Environment extends Canvas  {
+public class Environment extends Canvas {
     private List<Planet> planetsList;
     boolean running = false;
     Thread runner = null;
@@ -23,12 +23,12 @@ public class Environment extends Canvas  {
     private Vector3dInterface[] positionOfSpacechip;
 
     //old angle for the rocket = 0.5896979523584819, -0.8075660030972522, -0.00966827935790599
-    public Environment(){
+    public Environment() {
         Planets planets = new Planets();
-        this.planetsList=planets.getPlanets();
+        this.planetsList = planets.getPlanets();
         //0.5896979523584819, -0.8075660030972522, -0.0096682793579059
-        positionOfSpacechip = sim.trajectory(  new Vector(6371e3,0,0), new Vector(1.0519798448579987, 9.78871953507167, -9.152967539273916  ),31556926, 1000);
-       StateInterface[] arr = sim.getPositionOfPlanets(); // vector before  new Vector(-1.471922101663588e+11+6371e3,-2.860995816266412e+10,8.278183193596080e+06),new Vector(5.427193405797901e+03,-2.931056622265021e+04,6.575428158157592e-01)
+        positionOfSpacechip = sim.trajectory(new Vector(6371e3, 0, 0), new Vector(1.0519798448579987, 9.78871953507167, -9.152967539273916), 31556926, 1000);
+        StateInterface[] arr = sim.getPositionOfPlanets(); // vector before  new Vector(-1.471922101663588e+11+6371e3,-2.860995816266412e+10,8.278183193596080e+06),new Vector(5.427193405797901e+03,-2.931056622265021e+04,6.575428158157592e-01)
         positionsOfPlanets = new StateOfSolarSystem[arr.length];
         for (int m = 0; m < positionsOfPlanets.length; m++) {
             positionsOfPlanets[m] = (StateOfSolarSystem) arr[m];
@@ -36,25 +36,36 @@ public class Environment extends Canvas  {
     }
 
 
-    public void drawplease(Graphics g,int index) {
+    public void drawplease(Graphics g, int index) {
         //Adds all our planets and calls every individual draw from each planet
 //        g.setColor(Color.WHITE);
 //        g.drawString("hello",100,100);
 
         //System.out.println(index);
-       //System.out.println( positionsOfPlanets.length);
+        //System.out.println( positionsOfPlanets.length);
 
         Vector3dInterface[] pPlanets = positionsOfPlanets[index].getPositionOfPlanets();
 
 
-            for (int m = 0 ; m< planetsList.size(); m++) {
+        //SUN IN THE MIDDLE
 
-                planetsList.get(m).setX(pPlanets[m].getX());
-                planetsList.get(m).setY(pPlanets[m].getY());
-                planetsList.get(m).draw(g);
+//
+//            for (int m = 0 ; m< planetsList.size(); m++) {
+//
+//                planetsList.get(m).setX(pPlanets[m].getX());
+//                planetsList.get(m).setY(pPlanets[m].getY());
+//                planetsList.get(m).draw(g);
+//
+//
+//            }
 
+//SPACESHIP IN THE MIDDLE
 
-            }
+        for (int m = 0; m < planetsList.size(); m++) {
+            planetsList.get(m).setX(pPlanets[m].getX() - pPlanets[11].getX());
+            planetsList.get(m).setY(pPlanets[m].getY() - pPlanets[11].getY());
+            planetsList.get(m).draw(g);
+        }
         /*    if(planetsList.get(8).getInitialPosition().sub(planetsList.get(11).getInitialPosition()).norm() < 3e9)
             {
                 System.out.println("Sussy");
@@ -64,30 +75,30 @@ public class Environment extends Canvas  {
                 System.out.println("Amogus");
             } */
 
-            for(int m = 0 ; m< planetsList.size()-1; m++){
+        for (int m = 0; m < planetsList.size() - 1; m++) {
 
-                if(isInside(
-                        planetsList.get(m).getInitialPosition().getX(),
-                        planetsList.get(m).getInitialPosition().getY(),
-                        planetsList.get(m).getRadius(),
-                        planetsList.get(11).getInitialPosition().getX(),
-                        planetsList.get(11).getInitialPosition().getY())
-                ){
-                    System.out.println("");
-                    System.out.println("its in the "+planetsList.get(m).getName());
-                    System.out.println("");
-                }else{
-                  //  System.out.println(planetsList.get(m).getName()+" : "+planetsList.get(m).getInitialPosition());
-                    //System.out.println(planetsList.get(11).getName()+" : "+planetsList.get(11).getInitialPosition());
-                }
-
-
+            if (isInside(
+                    planetsList.get(m).getInitialPosition().getX(),
+                    planetsList.get(m).getInitialPosition().getY(),
+                    planetsList.get(m).getRadius(),
+                    planetsList.get(11).getInitialPosition().getX(),
+                    planetsList.get(11).getInitialPosition().getY())
+            ) {
+                System.out.println("");
+                System.out.println("its in the " + planetsList.get(m).getName());
+                System.out.println("");
+            } else {
+                //  System.out.println(planetsList.get(m).getName()+" : "+planetsList.get(m).getInitialPosition());
+                //System.out.println(planetsList.get(11).getName()+" : "+planetsList.get(11).getInitialPosition());
             }
+
+
+        }
         //draw the ship at the initial position
         //ship.draw(g, (int) ship.getPosition().getX(), (int) ship.getPosition().getY());
     }
 
-    public void run(int frames){
+    public void run(int frames) {
 
         BufferStrategy bs = this.getBufferStrategy();
         if (bs == null) {
@@ -100,7 +111,7 @@ public class Environment extends Canvas  {
         g.setColor(Color.black);
         g.fillRect(0, 0, 1500, 1000);
 
-        this.drawplease(g,frames);
+        this.drawplease(g, frames);
 
         g.dispose();
         bs.show();
@@ -120,7 +131,6 @@ public class Environment extends Canvas  {
         return (x - circle_x) * (x - circle_x) +
                 (y - circle_y) * (y - circle_y) <= rad * rad;
     }
-
 
 
 }
