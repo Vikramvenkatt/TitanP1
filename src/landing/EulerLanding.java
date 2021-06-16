71411e25545613a8cf9af2a0839dfa43e5cae072
Jan-04 <j.grapenthin@student.maastricht.university.nl>
2021-06-16 17:35:03 +0200

    Physics engine for landing fixed bugs and modified it, so it is usable with the controllers and the gui

--- src/landing/EulerLanding.java
+++ src/landing/EulerLanding.java
@@ -1,6 +1,7 @@
 package landing;
 
 import GenBody.Vector;
+import interfaces.Vector3dInterface;
 
 import static java.lang.Math.cos;
 import static java.lang.Math.sin;
@@ -11,18 +12,19 @@ public class EulerLanding {
     private Vector postStepPos;
     private Vector preStepVel;
     private Vector postStepVel;
-    private static double vMax; // max torque
-    private static double uMax;
 
-    public EulerLanding(Vector preStepPos, Vector preStepVel){
-        this.preStepPos = preStepPos;
-        this.preStepVel = preStepVel;
+    public EulerLanding(Vector3dInterface preStepPos, Vector3dInterface preStepVel){
+        this.preStepPos = (Vector) preStepPos;
+        this.preStepVel = (Vector) preStepVel;
     }
 
-    public Vector step(double v,double u, double h){
+    public void step(double v,double u, double h){
 
         double deltaTheta = preStepVel.getZ()+v*h;
         double theta = preStepPos.getZ()+deltaTheta*h;
+
+        if (theta>360)
+          theta = (int) Math.ceil((theta%360));
 
         double deltaX = preStepVel.getX()+ (u*sin(theta))*h;
         double x = preStepPos.getX()+deltaX*h;
@@ -30,13 +32,17 @@ public class EulerLanding {
         double deltaY = preStepVel.getY()+(u*cos(theta)-1.352)*h;
         double y = preStepPos.getY()+deltaY*h;
 
-        postStepPos = new Vector(deltaX,deltaY,deltaTheta);
-        postStepVel = new Vector(x,y,theta);
+        postStepVel = new Vector(deltaX,deltaY,deltaTheta);
+        postStepPos = new Vector(x,y,theta);
 
-        return postStepPos;
+        return;
     }
 
     public Vector getPostStepVel() {
         return postStepVel;
+    }
+
+    public Vector getPostStepPos() {
+        return postStepPos;
     }
 }
--- src/landing/SimulationLanding.java
+++ src/landing/SimulationLanding.java
@@ -1,16 +1,46 @@
 package landing;
+import GenBody.Vector;
 import interfaces.ProbeSimulatorInterface;
 import interfaces.Vector3dInterface;
 
-public class SimulationLanding implements ProbeSimulatorInterface {
+public class SimulationLanding  {
 
-    @Override
-    public Vector3dInterface[] trajectory(Vector3dInterface p0, Vector3dInterface v0, double[] ts) {
-        return new Vector3dInterface[0];
+    public static void main(String[] args) {
+        Vector[] n = trajectory(new Vector(8000,8000,90), new Vector(50000,0,90),1000,10);
+        for (int i = 0; i < n.length; i++) {
+         System.out.println(n[i].toString());
+        }
     }
 
-    @Override
-    public Vector3dInterface[] trajectory(Vector3dInterface p0, Vector3dInterface v0, double tf, double h) {
-        return new Vector3dInterface[0];
+
+    public static Vector[] trajectory(Vector3dInterface p0, Vector3dInterface v0, double tf, double h) {
+
+        Vector[] trajectory = new Vector[(int)Math.ceil(tf/h)+1];
+
+        EulerLanding n;
+
+        double time=0;
+
+        for (int i = 0; i < trajectory.length; i++) {
+
+            n = step(p0,v0,h);
+
+            p0 = n.getPostStepPos();
+
+            v0 = n.getPostStepVel();
+
+            trajectory[i] = (Vector) p0;
+
+            time+=h;
+
+        }
+        return  trajectory;
+    }
+
+    private static EulerLanding step(Vector3dInterface p0,Vector3dInterface v0, double h) {
+        EulerLanding n = new EulerLanding(p0,v0);
+        // TODO: add the controller here, to calculate v and u;
+        n.step(1,1,h);
+        return n;
     }
 }
