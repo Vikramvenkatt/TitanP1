1b310ec352dc52f0d4099891b3e5cb9def593c20
KaiNi21 <kaiw@live.fr>
2021-06-18 16:39:51 +0200

    AltWindModel updated

--- src/landing/AltWindModel.java
+++ src/landing/AltWindModel.java
@@ -8,7 +8,7 @@ public class AltWindModel {
 
     //multiplier = strength variation of wind as %
     //height is given as meters --> convert to km
-    public void simulateWind(double height, double multiplier)
+    public double simulateWind(double height, double multiplier)
     {
         height = height/1000;
         Random rand = new Random();
@@ -16,8 +16,31 @@ public class AltWindModel {
         double randomGauss = rand.nextGaussian();
         // insert Math.abs check for randomGauss to prevent multiplier > 1 or < 0
 
-        double windVelocity;
-        Vector direction;
+        //true = wind goes to the right, false = wind goes left
+        boolean direction = true;
+        double windVelocity = 0;  //in m/s
+        if(height > 120)
+        {
+            windVelocity = 0;
+        }
+        else if(height < 120 && height > 100)
+        {
+            windVelocity = 120;
+        }
+        else if(height < 100 && height > 60)
+        {
+            windVelocity = 5;
+        }
+        else if(height < 60)
+        {
+            windVelocity = 1;
+        }
+
+        if(direction)
+        {
+            return windVelocity;
+        }
+        else return -windVelocity;
 
     }
 }
--- src/landing/Main.java
+++ src/landing/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
         SimulationLanding k = new SimulationLanding();
-        Vector[] n = k.trajectory(new Vector(8000,8000,90), new Vector(-500,0,0),4000,1);
+        Vector[] n = k.trajectory(new Vector(8000,8000,0), new Vector(-500,0,0),4000,1);
         for (int i = 0; i < n.length; i++) {
             System.out.println(n[i].toString());
         }
